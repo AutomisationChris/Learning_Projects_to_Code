b4c5b14a9c06231daf6dc4a386946b71e3103d31
Chris <34416055+AutomisationChris@users.noreply.github.com>
2025-04-17 13:50:43 +0200

    Update streamlit_paris_events.py

--- 05_Streamlit_Apps/streamlit_paris_events.py
+++ 05_Streamlit_Apps/streamlit_paris_events.py
@@ -85,7 +85,7 @@ for event in daten['records']:
     col1, col2 = st.columns([2, 3])
     with col1:
         if event_pic:
-            st.markdown(f'<a href="{event_url}"><img src="{event_pic}"></a>', unsafe_allow_html=True)
+            st.markdown(f'<a href="{event_url}"><img src="{event_pic}" width="200"></a>', unsafe_allow_html=True)
         else:
             st.write("📸 Kein Bild vorhanden.")
 
