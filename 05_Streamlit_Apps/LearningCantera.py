b63033923048d9f97c927240a1c0e665d6d33acd
Chris <34416055+AutomisationChris@users.noreply.github.com>
2025-05-13 22:51:28 +0200

    Update LearningCantera.py

--- 05_Streamlit_Apps/LearningCantera.py
+++ 05_Streamlit_Apps/LearningCantera.py
@@ -1,4 +1,12 @@
 import cantera as ct
 import numpy as np
+import streamlit as st
+
 gas1 = ct.Solution('gri30.yaml')
 gas1()
+
+
+# Streamlit Interface
+st.title('Cantera Gas Simulation in Streamlit')
+
+st.write(gas1())
