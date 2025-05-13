fa5be4f79d05d01b3c2a0d60468b50561d31273e
Chris <34416055+AutomisationChris@users.noreply.github.com>
2025-05-14 00:42:17 +0200

    Update LearningCantera.py

--- 05_Streamlit_Apps/LearningCantera.py
+++ 05_Streamlit_Apps/LearningCantera.py
@@ -1,8 +1,9 @@
 import cantera as ct
 import numpy as np
 import streamlit as st
-gas1.TP = 1200, 101325
+
 gas1 = ct.Solution('gri30.yaml')
+gas1.TP = 1200, 101325
 cantera_output = str(gas1())
 
 # Streamlit Interface
